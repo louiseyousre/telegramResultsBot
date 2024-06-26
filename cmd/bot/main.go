900ac4951ed630b22a684a0def41d9bc2b34a36d
Louise Yousre <louiseyousre@outlook.com>
2024-06-27 02:43:24 +0300

    Attempt #1 to get it working

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 )
@@ -18,31 +17,26 @@ func main() {
 		bot.WithDefaultHandler(handler),
 	}
 
-	b, _ := bot.New(os.Getenv("TELEGRAM_BOT_TOKEN"), opts...)
-
-	// call methods.SetWebhook if needed
-
-	go b.StartWebhook(ctx)
-
-	log.Print("Starting the bot server...")
-
-	err := http.ListenAndServe(":2000", b.WebhookHandler())
+	b, err := bot.New(os.Getenv("TELEGRAM_BOT_TOKEN"), opts...)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// call methods.DeleteWebhook if needed
+	log.Print("Starting the bot server...")
 
+	b.Start(ctx)
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	log.Printf("Got a message %s, and will resend it back.", update.Message.Text)
 
-	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID: update.Message.Chat.ID,
-		Text:   update.Message.Text,
-	})
-	if err != nil {
-		log.Print(err)
+	if update.Message != nil {
+		_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   update.Message.Text,
+		})
+		if err != nil {
+			log.Print(err)
+		}
 	}
 }
