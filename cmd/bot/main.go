efbd099fc9e6baeeee0716f9dac474c204cec89b
Louise Yousre <louiseyousre@outlook.com>
2024-06-27 06:39:34 +0300

    Implemented bot main functionality

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
+	"telegramResultsBot/internal/portal"
 	"time"
 )
 
@@ -57,19 +60,254 @@ func main() {
 
 	go startHealthCheckServer(context.Background())
 
+	rbot := newResultsBot()
+
+	b.RegisterHandlerMatchFunc(matchGetResults, rbot.getResultsHandler)
+
 	log.Print("Starting the bot server...")
 
 	b.Start(ctx)
+
+}
+
+type resultsBot struct {
+	portalService *portal.Service
+}
+
+func newResultsBot() *resultsBot {
+	return &resultsBot{portalService: portal.NewService()}
+}
+
+func matchGetResults(update *models.Update) bool {
+	if update.Message == nil {
+		return false
+	}
+
+	text := strings.TrimSpace(update.Message.Text)
+	if text == "" {
+		return false
+	}
+
+	textLines := strings.Split(strings.ReplaceAll(text, "\r\n", "\n"), "\n")
+	if len(textLines) != 2 {
+		return false
+	}
+
+	return true
+}
+
+func replyParametersTo(msg *models.Message) *models.ReplyParameters {
+	if msg == nil {
+		return nil
+	}
+
+	return &models.ReplyParameters{
+		MessageID:                msg.ID,
+		ChatID:                   msg.Chat.ID,
+		AllowSendingWithoutReply: true,
+	}
+}
+
+func (r *resultsBot) getResultsHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	textLines := strings.Split(strings.ReplaceAll(strings.TrimSpace(update.Message.Text), "\r\n", "\n"), "\n")
+
+	nationalID := strings.TrimSpace(textLines[0])
+	if len(nationalID) != 16 {
+		_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:          update.Message.Chat.ID,
+			Text:            "السطر الأول في الرسالة اللي بعتها مفيهاش رقم قومي صح لأنه المفروض يبقى 16 رقم. إتأكد تاني وابعت من جديد.",
+			ReplyParameters: replyParametersTo(update.Message),
+		})
+
+		if err != nil {
+			log.Print(err)
+		}
+
+		return
+	}
+
+	password := strings.TrimSpace(textLines[1])
+	if len(password) == 0 {
+		_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:          update.Message.Chat.ID,
+			Text:            "السطر التاني مفيهوش باسورد. إتأكد إنك مش حاطط مثلاً مسافات أو حاجة شبه كدا وإبعت تاني من جديد.",
+			ReplyParameters: replyParametersTo(update.Message),
+		})
+
+		if err != nil {
+			log.Print(err)
+		}
+
+		return
+	}
+
+	message, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:          update.Message.Chat.ID,
+		Text:            "ثواني بنحاول نسجل الدخول...",
+		ReplyParameters: replyParametersTo(update.Message),
+	})
+	if err != nil {
+		log.Print(err)
+	}
+
+	var cookie *http.Cookie
+	cookie, err = r.portalService.Login(nationalID, password)
+	if err != nil {
+		log.Print(err)
+
+		if errors.Is(err, portal.ErrInvalidCredentials) {
+			_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID:          update.Message.Chat.ID,
+				Text:            "راجع بيانات الدخول تاني كدا الموقع مش قابلها. إفتكر إن الرقم القومي تكتبه في السطر الأول والباسورد في السطر التاني.",
+				ReplyParameters: replyParametersTo(update.Message),
+			})
+		} else {
+			_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID:          message.ID,
+				Text:            "معلهش متأسفين. حصل خطأ مش عارفينه إيه هو بالظبط ولكن هنحاول نشوفه إيه هو ولو ينفع يتصلح من عندنا هنصلحه.",
+				ReplyParameters: replyParametersTo(message),
+			})
+		}
+
+		if err != nil {
+			log.Print(err)
+		}
+
+		return
+	}
+	message, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:          update.Message.Chat.ID,
+		Text:            "تسجيل الدخول نجح. لحظات نجيب بياناتك...",
+		ReplyParameters: replyParametersTo(message),
+	})
+	if err != nil {
+		log.Print(err)
+	}
+
+	var data *portal.StudentData
+	data, err = r.portalService.GetStudentData(cookie)
+	if err != nil {
+		log.Print(err)
+
+		_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:          update.Message.Chat.ID,
+			Text:            "معلهش متأسفين. حصل خطأ مش عارفينه إيه هو بالظبط ولكن هنحاول نشوفه إيه هو ولو ينفع يتصلح من عندنا هنصلحه.",
+			ReplyParameters: replyParametersTo(message),
+		})
+
+		return
+	}
+
+	studentName, err := portal.GetFirstTranslation(data.StdName)
+	if err != nil {
+		log.Printf("Student name is malformed: %s", data.StdName)
+
+		_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:          update.Message.Chat.ID,
+			Text:            "البيانات اللي جات من الموقع مش سليمة ممكن يكون فيه مشكلة دلوقتي أو يكون البوت قدم والموقع حصل فيه تغييرات.",
+			ReplyParameters: replyParametersTo(message),
+		})
+
+		if err != nil {
+			log.Print(err)
+		}
+
+		return
+	}
+
+	message, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:          update.Message.Chat.ID,
+		Text:            fmt.Sprintf("جبنا بياناتك بنجاح يا %s، حد قالك إنك حد جميل وشكلك حلو ❤️ ثواني هنجيب النتيجة بقى.", *studentName),
+		ReplyParameters: replyParametersTo(message),
+	})
+	if err != nil {
+		log.Print(err)
+	}
+
+	var results *[]portal.StudentResult
+	results, err = r.portalService.GetResults(cookie, data.UUID)
+	if err != nil {
+		log.Print(err)
+		_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID:          update.Message.Chat.ID,
+			Text:            "معلهش متأسفين. حصل خطأ مش عارفينه إيه هو بالظبط ولكن هنحاول نشوفه إيه هو ولو ينفع يتصلح من عندنا هنصلحه.",
+			ReplyParameters: replyParametersTo(message),
+		})
+		return
+	}
+
+	var formattedResults *string
+	formattedResults, err = portal.FormatResults(results)
+	if err != nil {
+		log.Print(err)
+
+		if errors.Is(err, portal.ErrMalformedResults) {
+			_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID:          update.Message.Chat.ID,
+				Text:            "النتيجة اللي جات من الموقع مش سليمة ممكن يكون فيه مشكلة دلوقتي أو يكون البوت قدم والموقع حصل فيه تغييرات.",
+				ReplyParameters: replyParametersTo(message),
+			})
+		} else {
+			_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+				ChatID:          update.Message.Chat.ID,
+				Text:            "معلهش متأسفين. حصل خطأ مش عارفينه إيه هو بالظبط ولكن هنحاول نشوفه إيه هو ولو ينفع يتصلح من عندنا هنصلحه.",
+				ReplyParameters: replyParametersTo(message),
+			})
+		}
+
+		return
+	}
+
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID:          update.Message.Chat.ID,
+		Text:            *formattedResults,
+		ReplyParameters: replyParametersTo(message),
+	})
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	if update.Message != nil {
 		_, err := b.SendMessage(ctx, &bot.SendMessageParams{
-			ChatID: update.Message.Chat.ID,
-			Text:   update.Message.Text,
+			ChatID:          update.Message.Chat.ID,
+			Text:            "بص يا صديقي البوت دا مبيفهمش أي حاجة غير إنك تكتب الرقم القومي في السطر الأول والباسورد في السطر التاني، وبكدا هيحاول يجيب النتيجة بتاعتك ويبعتهالك.",
+			ReplyParameters: replyParametersTo(update.Message),
 		})
 		if err != nil {
 			log.Print(err)
 		}
 	}
 }
+
+//func getStudentPhoto(ctx context.Context, chatId int64, imagePath string) (*bot.SendPhotoParams, error) {
+//	// Make a GET request to download the image
+//	resp, err := http.Get("http://stda.minia.edu.eg" + imagePath)
+//	if err != nil {
+//		return nil, fmt.Errorf("error downloading image, %v\n", err)
+//	}
+//	defer func(resp *http.Response) {
+//		_ = resp.Body.Close()
+//	}(resp)
+//
+//	// Check if the request was successful
+//	if resp.StatusCode != http.StatusOK {
+//		return nil, fmt.Errorf("error: received non-200 response code %d\n", resp.StatusCode)
+//	}
+//
+//	// Read the image data from the response body
+//	var imageData []byte
+//	imageData, err = io.ReadAll(resp.Body)
+//	if err != nil {
+//		return nil, fmt.Errorf("error reading image data, %v\n", err)
+//	}
+//
+//	// Create a new reader for the image data
+//	imageReader := bytes.NewReader(imageData)
+//
+//	// Prepare the parameters for sending the photo
+//	return &bot.SendPhotoParams{
+//		ChatID:  chatId,
+//		Photo:   &models.InputFileUpload{Filename: "student_photo.jpg", Data: imageReader},
+//		Caption: "صورة أجمل طالب",
+//	}, nil
+//
+//}
---
+++ internal/portal/service.go
@@ -0,0 +1,314 @@
+package portal
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const (
+	authCookieName = "PortalStudentUserID"
+	loginEndpoint  = "http://stda.minia.edu.eg/Portallogin"
+	getJCIEndpoint = "http://stda.minia.edu.eg/PortalgetJCI"
+)
+
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrUnexpectedNil      = errors.New("unexpected nil value")
+	ErrMalformedResults   = errors.New("malformed results")
+)
+
+type Service struct {
+	httpClient *http.Client
+}
+
+func NewService() *Service {
+	return &Service{
+		httpClient: &http.Client{},
+	}
+}
+
+func createLoginRequest(username, password string) (*http.Request, error) {
+	data := url.Values{
+		"UserName": {username},
+		"Password": {password},
+	}
+
+	req, err := http.NewRequest("POST", loginEndpoint, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+
+	return req, nil
+}
+
+func (p *Service) Login(username, password string) (*http.Cookie, error) {
+	req, err := createLoginRequest(username, password)
+	if err != nil {
+		return nil, fmt.Errorf("error creating login request: %v", err)
+	}
+
+	var resp *http.Response
+	resp, err = p.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer func(resp *http.Response) {
+		_ = resp.Body.Close()
+	}(resp)
+
+	cookies := resp.Cookies()
+
+	for _, cookie := range cookies {
+		if cookie.Name == authCookieName && cookie.Value != "" {
+			return cookie, nil
+		}
+	}
+
+	return nil, ErrInvalidCredentials
+}
+
+type StudentResult struct {
+	ScopeName string `json:"ScopeName"`
+	Year      string `json:"Year"`
+	Ds        []struct {
+		GradeName        *string `json:"GradeName"`
+		Percent          *string `json:"Percent"`
+		Total            *string `json:"Total"`
+		StudyYearCourses []struct {
+			GradeName string `json:"GradeName"`
+			Parts     []struct {
+				DegreesType    []string `json:"DegreesType"`
+				DegreesMax     string   `json:"DegreesMax"`
+				Degrees        []string `json:"Degrees"`
+				CoursePartName string   `json:"CoursePartName"`
+				SemasterName   string   `json:"SemasterName"`
+			} `json:"Parts"`
+			CourseName  string `json:"CourseName"`
+			SuccessFlag string `json:"SuccessFlag"`
+			Max         string `json:"Max"`
+			Total       string `json:"Total"`
+		} `json:"StudyYearCourses"`
+	} `json:"ds"`
+}
+
+func createGetResultsRequest(cookie *http.Cookie, uuid string) (*http.Request, error) {
+	data := url.Values{
+		"param0": {"Portal.Results"},
+		"param1": {"GetAllResults"},
+		"param2": {fmt.Sprintf("{\"UUID\":\"%s\"}", uuid)},
+	}
+
+	log.Print(bytes.NewBufferString(data.Encode()).String())
+
+	req, err := http.NewRequest("POST", getJCIEndpoint, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+
+	req.AddCookie(cookie)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+
+	return req, nil
+}
+
+func (p *Service) GetResults(cookie *http.Cookie, uuid string) (*[]StudentResult, error) {
+	req, err := createGetResultsRequest(cookie, uuid)
+
+	var resp *http.Response
+	resp, err = p.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer func(resp *http.Response) {
+		_ = resp.Body.Close()
+	}(resp)
+
+	var body []byte
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %w", err)
+	}
+
+	var response []StudentResult
+	if err = json.Unmarshal(body, &response); err != nil {
+		return nil, fmt.Errorf("error parsing response: %w", err)
+	}
+
+	return &response, nil
+
+}
+
+func GetFirstTranslation(str string) (*string, error) {
+	if res := strings.Split(str, "|"); len(res) >= 1 {
+		return &res[0], nil
+	} else {
+		return nil, ErrMalformedResults
+	}
+}
+
+func FormatResults(results *[]StudentResult) (*string, error) {
+	if results == nil {
+		return nil, ErrUnexpectedNil
+	}
+
+	if len(*results) == 0 {
+		msg := "مفيش درجات ولا حاجة لسة يا صديقي!"
+		return &msg, nil
+	}
+
+	var builder strings.Builder
+
+	for resultsIndex, result := range *results {
+		if name, err := GetFirstTranslation(result.ScopeName); err == nil {
+			builder.WriteString(fmt.Sprintf("⬅ %s (%s)", *name, result.Year))
+		} else {
+			return nil, ErrMalformedResults
+		}
+
+		if len(result.Ds) < 1 {
+			return nil, ErrMalformedResults
+		}
+
+		yearResult := result.Ds[0]
+
+		if yearResult.Percent != nil && yearResult.GradeName != nil && yearResult.Total != nil {
+			gradeName, err := GetFirstTranslation(*yearResult.GradeName)
+			if err != nil {
+				return nil, err
+			}
+
+			builder.WriteString(fmt.Sprintf(
+				" - (التقدير العام: %s) - (المجموع الكلي: %s) - (النسبة المئوية الكلية: %s%%)",
+				*gradeName,
+				*yearResult.Total,
+				*yearResult.Percent,
+			))
+		}
+
+		builder.WriteString("\n")
+
+		for courseIndex, course := range yearResult.StudyYearCourses {
+			courseName, err := GetFirstTranslation(course.CourseName)
+			if err != nil {
+				return nil, err
+			}
+
+			gradeName, err := GetFirstTranslation(course.GradeName)
+			if err != nil {
+				return nil, err
+			}
+
+			details := fmt.Sprintf(
+				"%d) %s - %s - %s/%s",
+				courseIndex+1,
+				*courseName,
+				*gradeName,
+				course.Max,
+				course.Total,
+			)
+
+			builder.WriteString(details)
+
+			if len(course.Parts) > 0 {
+				var partsBuilder strings.Builder
+
+				for _, part := range course.Parts {
+					if len(part.Degrees) != len(part.DegreesType) {
+						continue
+					}
+
+					for degreeIndex, degree := range part.Degrees {
+						var degreeType *string
+						degreeType, err = GetFirstTranslation(part.DegreesType[degreeIndex])
+						if err != nil {
+							return nil, err
+						}
+
+						partsBuilder.WriteString(fmt.Sprintf("%s = %s", *degreeType, degree))
+
+						if degreeIndex != len(part.Degrees)-1 {
+							partsBuilder.WriteString(", ")
+						}
+					}
+				}
+
+				partsString := partsBuilder.String()
+				if partsString != "" {
+					builder.WriteString(fmt.Sprintf(" (%s)", partsString))
+				}
+			}
+
+			builder.WriteString("\n")
+		}
+
+		if resultsIndex != len(*results)-1 {
+			builder.WriteString("\n\n\n")
+		}
+	}
+
+	resultString := builder.String()
+
+	return &resultString, nil
+}
+
+type StudentData struct {
+	CollageID   string `json:"CollageID"`
+	ImagePath   string `json:"ImagePath"`
+	UUID        string `json:"UUID"`
+	Collage     string `json:"Collage"`
+	ScopeUUID   string `json:"ScopeUUID"`
+	StdName     string `json:"StdName"`
+	Year        string `json:"Year"`
+	ShowMessage string `json:"ShowMessage"`
+	ID          int64  `json:"ID"`
+	StudyYear   string `json:"StudyYear"`
+}
+
+func (p *Service) GetStudentData(cookie *http.Cookie) (*StudentData, error) {
+	data := url.Values{
+		"param0": {"Portal.General"},
+		"param1": {"GetStudentPortalData"},
+		"param2": {"{\"UserID\":\"\"}"},
+	}
+
+	req, err := http.NewRequest("POST", getJCIEndpoint, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		return nil, fmt.Errorf("error creating request: %w", err)
+	}
+
+	req.AddCookie(cookie)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+
+	var resp *http.Response
+	resp, err = p.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer func(resp *http.Response) {
+		_ = resp.Body.Close()
+	}(resp)
+
+	var body []byte
+	body, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response: %w", err)
+	}
+
+	response := &[]StudentData{}
+	if err = json.Unmarshal(body, response); err != nil {
+		return nil, fmt.Errorf("error parsing response: %w", err)
+	}
+
+	return &(*response)[0], nil
+}
